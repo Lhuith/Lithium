418c6bdfe79bb39f7216070221649ce56214b097
Eugene Martens <genejmartens@gmail.com>
2020-12-13 15:08:16 +1000

    instance geometry class's imported

---
+++ custom_modules/core/data/instance_geometry/instance_attribute.js
@@ -0,0 +1,184 @@
+class state {
+    constructor(){
+        this.active = false;
+    }
+}
+
+export class instance_attribute {
+    type = "instance_attributes"
+    constructor(array, index){
+        if(array != undefined) {
+            console.warn("array was undefined, creating one instead.")
+            this.populate(array, index)
+        }
+    }
+    populate = (array, index) => {
+        var states = []
+        for(var i = 0; i < index; i++){
+            states.push(false)
+        }
+        this.states =  new THREE.InstancedBufferAttribute(new Float32Array(states), 1);
+
+        //this.translation = array[0]; // ✓
+        this.orientation = array[0]; // ✓
+
+        // color
+        this.col = array[1]; // ✓
+
+        this.uvoffset = array[2]; // ✓
+        this.tile_size = array[3]; // ✓
+        this.scale = array[4]; // ✓
+
+        // animations
+        this.animation_start = array[5]; // ✓
+        this.animation_end = array[6];  // ✓
+        this.animation_time = array[7]; // ✓
+        
+        this.type = array[8]; // ✓
+        this.fog = array[9]; // ✓
+        
+        //transform
+        this.m0 = array[10]; // ✓
+        this.m1 = array[11]; // ✓
+        this.m2 = array[12]; // ✓
+        this.m3 = array[13]; // ✓
+
+        this.index = 0;
+        this.max = index;
+    }
+    set = (decomposer) => {
+        var index = 0;
+    
+        // grab the first empty slot in the buffer
+        while (this.states.getX(index) != 0 && index < this.max){ index++ }
+        
+        this.states.setX(index, true)
+
+        var uvs = decomposer.ssIndex[randomRangeRound(0, decomposer.ssIndex.length - 1)];
+        this.set_uvoffset(index, uvs);
+
+        this.set_tile_size(index, decomposer.tile_size);
+
+        //var vector = new THREE.Vector4(
+        //    decomposer.position.x, 
+        //    decomposer.position.y, 
+        //    decomposer.position.z, 0).normalize();
+        //
+        //
+        //var translation = new THREE.Vector3(
+        //    decomposer.position.x + vector.x, 
+        //    decomposer.position.y + vector.y, 
+        //    decomposer.position.z + vector.z
+        //);
+        //
+        //this.set_translation(index, translation);
+        this.set_orientation(index, decomposer.orient);
+        this.set_scale(index, decomposer.scale);
+        this.set_type(index, decomposer.type);
+        this.set_fog(index, decomposer.fog);
+
+        var col = decomposer.colors[randomRangeRound(0, decomposer.colors.length - 1)];
+        var col_vector = new THREE.Vector4(col.r, col.g, col.b, 1.0);
+
+        this.set_color(index, col_vector)
+
+        this.set_transform(index, decomposer.matrix)
+
+        decomposer.buffer_idx = index;
+    }
+    unset = (index) => {
+        //console.log("attributes being reset?", index)
+        this.set_uvoffset(index, new THREE.Vector2(0,0));
+        this.set_tile_size(index, new THREE.Vector2(0,0));
+    
+        this.set_orientation(index, new THREE.Vector4(0,0,0,0));
+        this.set_scale(index, new THREE.Vector3(1,1,1));
+        this.set_type(index, 0);
+        this.set_fog(index, 0);
+        var col_vector = new THREE.Vector3(0, 0, 0);
+        this.set_color(index, col_vector)
+    
+        //this.set_animation(index, )
+        this.set_transform(index, new THREE.Matrix4())
+        this.states.setX(index, false)
+    }
+    set_uvoffset = (index, uv) => {
+        this.uvoffset.setXY(index, uv.x, uv.y);
+        this.uvoffset.needsUpdate = true;
+    }
+    set_tile_size = (index, ts) => {
+        this.tile_size.setXY(index, ts.x, ts.y);
+        this.tile_size.needsUpdate = true;
+    }
+    set_translation = (index, t) => {
+        this.translation.setXYZ(index, t.x, t.y, t.z);
+        this.translation.needsUpdate = true;
+    }
+    set_orientation = (index, o) => {
+        this.orientation.setXYZW(index, o.x, o.y, o.z, o.w);
+        this.orientation.needsUpdate = true;
+    }
+    set_scale = (index, s) => {
+        this.scale.setXYZ(index, s.x, s.y, s.z);
+        this.scale.needsUpdate = true;
+    }
+    set_type = (index, type) => {
+        this.type.setX(index, type);
+        this.type.needsUpdate = true;
+    }
+    set_fog = (index, fog) => {
+        this.fog.setX(index, fog);
+        this.fog.needsUpdate = true;
+    }
+    set_color = (index, col) => {
+        this.col.setXYZW(index, col.x, col.y, col.z, col.w);
+        this.col.needsUpdate = true;
+    }
+    set_alpha = (index, alpha) => {
+        this.col.setW(index, alpha);
+        this.col.needsUpdate = true;
+    }
+    set_animation = (index, s, e, t) => {
+        this.animation_start.setX  (index, s);
+        this.animation_end.setX  (index, e);
+        this.animation_time.setX  (index, t);
+    
+        this.animation_start.needsUpdate = true;
+        this.animation_end.needsUpdate = true;
+        this.animation_time.needsUpdate = true;
+    }
+    set_transform = (index, matrix) => {
+        this.m0.setXYZW(
+            index,  
+            matrix.elements[0],
+            matrix.elements[1],
+            matrix.elements[2],
+            matrix.elements[3]
+        );
+        this.m1.setXYZW(
+            index,  
+            matrix.elements[4],
+            matrix.elements[5],
+            matrix.elements[6],
+            matrix.elements[7]
+        );
+        this.m2.setXYZW(
+            index,  
+            matrix.elements[8],
+            matrix.elements[9],
+            matrix.elements[10],
+            matrix.elements[11]
+        );
+        this.m3.setXYZW(
+            index,  
+            matrix.elements[12],
+            matrix.elements[13],
+            matrix.elements[14],
+            matrix.elements[15]
+        );
+        this.m0.needsUpdate = true;
+        this.m1.needsUpdate = true;
+        this.m2.needsUpdate = true;
+        this.m3.needsUpdate = true;
+    }
+}
---
+++ custom_modules/core/data/instance_geometry/instance_renderer.js
@@ -0,0 +1,161 @@
+export class instance_renderer {
+    type = "instance_renderer"
+
+    constructor(map_index, mesh, animate, is3D = false, shader){
+        this.buffer = new instance_buffer(true);
+        this.attributes = new instance_attributes();
+        this.mesh = mesh;
+        this.map_index = map_index;
+        
+        this.shader = null;
+    
+        if(shader == undefined){
+            //console.warn("shader not found, using defualt!")
+            this.shader = get_data("instance_shader");
+        } else {
+            this.shader = shader;
+        }
+    
+        this.animate = animate;
+        this.is3D = is3D
+    }
+    get_buffer = () => {
+        return this.buffer;
+    }
+    get_attributes = () => {
+        return this.attributes;
+    }
+    buffer_append = (o) => {
+        if(o != undefined){
+            if(o.get_component("decomposer") != null){
+                this.buffer.append(o.get_component("decomposer"))
+            }
+        }
+    }
+    bake_buffer = () => {
+        if (this.buffer == undefined || this.buffer.length == 0) {
+            console.error("buffer error.")
+        }
+    
+        if(this.buffer.index <= 0){
+            console.log("buffer is empty!")
+            return
+        }
+    
+        var bufferGeometry = new THREE.PlaneBufferGeometry(1, 1, 1); 
+        bufferGeometry.castShadow = true;
+    
+        var geometry = new THREE.InstancedBufferGeometry();
+        geometry.index = bufferGeometry.index;
+        geometry.attributes.position = bufferGeometry.attributes.position;
+        geometry.attributes.uv = bufferGeometry.attributes.uv;
+      
+        //var translationAttribute = new THREE.InstancedBufferAttribute(new Float32Array(this.buffer.translation), 3);
+        var orientationAttribute = new THREE.InstancedBufferAttribute(new Float32Array(this.buffer.orientations), 4);
+        var colorAttribute = new THREE.InstancedBufferAttribute(new Float32Array(this.buffer.colors), 4);
+        var uvOffsetAttribute = new THREE.InstancedBufferAttribute(new Float32Array(this.buffer.uvoffsets), 2);
+        var tileSizeAttribute = new THREE.InstancedBufferAttribute(new Float32Array(this.buffer.tile_size), 2);
+        var scaleAttribute = new THREE.InstancedBufferAttribute(new Float32Array(this.buffer.scales), 3);
+        var animation_startAttribute = new THREE.InstancedBufferAttribute(new Float32Array(this.buffer.animation_start), 1);
+        var animation_endAttribute = new THREE.InstancedBufferAttribute(new Float32Array(this.buffer.animation_end), 1);
+        var animation_timeAttribute = new THREE.InstancedBufferAttribute(new Float32Array(this.buffer.animation_time), 1);
+        var typeAttribute = new THREE.InstancedBufferAttribute(new Float32Array(this.buffer.type), 1);
+        var fogAttribute = new THREE.InstancedBufferAttribute(new Float32Array(this.buffer.fog), 1);
+        var m0Attribute = new THREE.InstancedBufferAttribute(new Float32Array(this.buffer.m0), 4);
+        var m1Attribute = new THREE.InstancedBufferAttribute(new Float32Array(this.buffer.m1), 4);
+        var m2Attribute = new THREE.InstancedBufferAttribute(new Float32Array(this.buffer.m2), 4);
+        var m3Attribute = new THREE.InstancedBufferAttribute(new Float32Array(this.buffer.m3), 4);
+    
+        //geometry.setAttribute('translation', translationAttribute);
+        geometry.setAttribute('orientation', orientationAttribute);
+        geometry.setAttribute('col', colorAttribute);
+        geometry.setAttribute('uvoffset', uvOffsetAttribute);
+        geometry.setAttribute('tile_size', tileSizeAttribute);
+        geometry.setAttribute('scale', scaleAttribute);
+        geometry.setAttribute('animation_start', animation_startAttribute);
+        geometry.setAttribute('animation_end', animation_endAttribute);
+        geometry.setAttribute('animation_time', animation_timeAttribute);
+        geometry.setAttribute('type', typeAttribute);
+        geometry.setAttribute('fog', fogAttribute);
+        geometry.setAttribute('m0', m0Attribute);
+        geometry.setAttribute('m1', m1Attribute);
+        geometry.setAttribute('m2', m2Attribute);
+        geometry.setAttribute('m3', m3Attribute);
+        
+        this.attributes.populate(
+            [
+                //translationAttribute,
+                orientationAttribute,
+                colorAttribute,
+                uvOffsetAttribute,
+                tileSizeAttribute,
+                scaleAttribute,
+                animation_startAttribute,
+                animation_endAttribute,
+                animation_timeAttribute,
+                typeAttribute,
+                fogAttribute,
+                m0Attribute,
+                m1Attribute,
+                m2Attribute,
+                m3Attribute,
+            ], this.buffer.index
+        );
+    
+        var texture = new THREE.TextureLoader().load(renderer_text_info[this.map_index].map);
+    
+        texture.magFilter = THREE.NearestFilter;
+        texture.minFilter = THREE.NearestFilter;
+    
+        var animationSwitch = 0;
+        var is3DSwitch = 0;
+    
+        if (this.animate)
+            animationSwitch = 1.0;
+    
+        if(this.is3D)
+            is3DSwitch = 1.0;
+    
+        var instanceUniforms = {
+            map: { value: texture },
+            spriteSheetX: { type: "f", value: SPRITE_SHEET_SIZE.x },
+            spriteSheetY: { type: "f", value: SPRITE_SHEET_SIZE.y },
+            animationSwitch: { type: "f", value: animationSwitch },
+            is3D: { type: "f", value: is3DSwitch },
+            time: { type: "f", value: 1.0 },
+        }
+    
+        if(this.shader == undefined){
+            console.error("shader wasnt found, using defualt.");
+            this.shader = get_data("instance_shader");
+        }
+    
+        var material = new THREE.RawShaderMaterial({
+            uniforms:
+                THREE.UniformsUtils.merge([
+                    THREE.UniformsLib['lights'],
+                    THREE.UniformsLib['fog'],
+                    instanceUniforms
+                ]),
+    
+            vertexShader: this.shader.vert,
+            fragmentShader:  this.shader.frag,
+            wireframe:  this.shader.extra.wf,
+            transparent:  this.shader.extra.trans,
+            fog: true,
+            lights: true,
+        });
+    
+        mesh = new THREE.Mesh(geometry, material);
+        material.uniforms.map.value = texture;
+        material.uniforms.spriteSheetX.value = SPRITE_SHEET_SIZE.x;
+        material.uniforms.spriteSheetY.value = SPRITE_SHEET_SIZE.y;
+    
+        material.side = THREE.DoubleSide;
+        mesh.frustumCulled = false;
+        mesh.castShadow = true;
+        //console.log("objects baked: ", this.buffer.index)
+    
+        this.mesh.add(mesh);
+    }
+}
