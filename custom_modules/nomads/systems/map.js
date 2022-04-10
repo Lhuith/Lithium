102c74ed391f898210fdbe5b47a5bcfcf9b017b8
Togene <jhonnywhiplash@hotmail.com.au>
2022-04-10 17:25:40 +1000

    minimap updates

--- custom_modules/nomads/systems/map.js
+++ custom_modules/nomads/systems/map.js
@@ -3,24 +3,25 @@ import * as keyboard from '/core/input/keyboard.js'
 
 let dx = 0;
 let dy = 0;
-let map_context;
+let map_context, map_context_element;
 let atlas;
 
 export const init = (three) => {
     console.log("%cMap Initialized", "color:#FFD9E5")
 
-    let map_context_element = document.getElementById('map');
+    map_context_element = document.getElementById('map');
     map_context = map_context_element.getContext('2d')
 
     map_context_element.style.backgroundColor = 'black'
-    map_context_element.width  = 200;
-    map_context_element.height = 200;
+    map_context_element.width  = 150;
+    map_context_element.height = 150;
 
     atlas = new Image();
     atlas.src = '/data/img/tile/Crab_Island/Crab_Island_color.png';
 
     atlas.onload = function() {
-        map_context.drawImage(atlas,0, 0);
+        console.log(atlas.width, atlas.height)
+        drawImageToMap(dx, dy)
     }
 }
 
@@ -31,9 +32,9 @@ export const update = (delta) => {
     if (keyboard.input.x) {
         dx--
     }
+    drawImageToMap(dx,dy)
+}
 
-
-    let map_context_element = document.getElementById('map');
-    map_context = map_context_element.getContext('2d')
-    map_context.drawImage(atlas, dx, dy);
+const drawImageToMap = (dx, dy) => {
+    map_context.drawImage(atlas, dx, dy, 32, 32);
 }
