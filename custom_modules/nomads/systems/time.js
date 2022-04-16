f23246e99fac81acd39239a3f142ac8343b58264
Togene <jhonnywhiplash@hotmail.com.au>
2022-04-17 02:23:46 +1000

    started game time

--- custom_modules/nomads/systems/time.js
+++ custom_modules/nomads/systems/time.js
@@ -1,14 +1,24 @@
-let game_time = 0
-let day_length = 200 // length in second
-export const init = (three, data) => {
+let game_second = 0
+let game_minute = 0
+let game_hour = 0
+
+let time_fraction = 100
+let divisor = 60/time_fraction
+
+let day_length = 86400/time_fraction // length in second
+
+export const init = () => {
     console.log("%cTime Initialized", "color:#eb33b5")
+    console.log("length of day: ", (day_length))
 }
 
 export const update = (delta) => {
-    game_time += delta
+    game_second += delta
+    game_minute = game_second/divisor
+    game_hour = game_minute/divisor
 
-    if (game_time > day_length) {
+    if (game_second >= day_length) {
         console.log("new day")
-        game_time = 0
+        game_second = 0
     }
 }
