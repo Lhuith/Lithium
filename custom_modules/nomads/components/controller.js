583d2065af6f4681bb61fac11a18e594b95a15b0
Togene <jhonnywhiplash@hotmail.com.au>
2022-06-07 22:44:55 +1000

    controller cleanup

--- custom_modules/nomads/components/controller.js
+++ custom_modules/nomads/components/controller.js
@@ -1,7 +1,7 @@
-import { component } from '/nomads/components/component.js';
-import {Vector3, Euler} from '/build/three.module.js';
-import {PointerLockControls} from '/jsm/controls/PointerLockControls.js';
-import * as keyboard from '/core/input/keyboard.js';
+import { component } from '/nomads/components/component.js'
+import {Vector3, Euler} from '/build/three.module.js'
+import {PointerLockControls} from '/jsm/controls/PointerLockControls.js'
+import * as keyboard from '/core/input/keyboard.js'
 import { quaternion } from '/core/math/quaternion.js'
 import { get_data } from '/core/data/antlion.js'
 
@@ -9,10 +9,10 @@ import * as file from '/core/meta/helpers/ajax.js'
 
 export class controller extends component {
     type = "controller"
-    required = ["transform"];
+    required = ["transform"]
     
     constructor(three){
-        super();
+        super()
     
         console.log("%cController Initialized", "color:#7d57c1")
         
@@ -24,11 +24,11 @@ export class controller extends component {
         }, false ) 
         this.controls = controller
 
-        this.speed = 10.25;
-        this.speed_mult = 4.5;
-        this.direction = new Vector3();
+        this.speed = 10.25
+        this.speed_mult = 4.5
+        this.direction = new Vector3()
 
-        var saved_player_information = JSON.parse(get_data("player").data)
+        let saved_player_information = JSON.parse(get_data("player").data)
         
         this.controls.getObject().position.x = saved_player_information.position.x
         this.controls.getObject().position.y = saved_player_information.position.y
@@ -42,55 +42,57 @@ export class controller extends component {
         this.movement(delta)
     }
     movement(delta){
-        this.direction.z = Number( keyboard.input.w ) - Number( keyboard.input.s);
-        this.direction.x = (Number( keyboard.input.a ) - Number( keyboard.input.d)) * -1;
-        this.direction.y = Number( keyboard.input.q ) - Number( keyboard.input.e);
+        this.direction.z = Number( keyboard.input.w ) - Number( keyboard.input.s)
+        this.direction.x = (Number( keyboard.input.a ) - Number( keyboard.input.d)) * -1
+        this.direction.y = Number( keyboard.input.q ) - Number( keyboard.input.e)
 
-        this.direction.normalize(); 
+        this.direction.normalize() 
 
         if(keyboard.input.shift) { 
-            this.speed_mult = 12.1;
+            this.speed_mult = 12.1
         } else { 
-            this.speed_mult = 1;
+            this.speed_mult = 1
         }
 
         if ((keyboard.input.q || keyboard.input.e)){
-           this.controls.getObject().position.y +=  (this.direction.y * (this.speed * this.speed_mult) * delta);
+           this.controls.getObject().position.y +=  (this.direction.y * (this.speed * this.speed_mult) * delta)
         } 
         if ((keyboard.input.w || keyboard.input.s)){
-            this.controls.moveForward(this.direction.z * (this.speed * this.speed_mult) * delta);
+            this.controls.moveForward(this.direction.z * (this.speed * this.speed_mult) * delta)
         } 
         if ((keyboard.input.a || keyboard.input.d)){
-            this.controls.moveRight(this.direction.x * (this.speed * this.speed_mult) * delta);
+            this.controls.moveRight(this.direction.x * (this.speed * this.speed_mult) * delta)
         }
 
         if(keyboard.input.space){
         }
         
         if (this.controls.getObject().position.y === 0.0 ) {
-           // canJump = true;
+           // canJump = true
         }
 
         this.parent.transform.position = 
         new Vector3(
             this.controls.getObject().position.x, 
             this.controls.getObject().position.y - 0.25, 
-            this.controls.getObject().position.z - 1)
+            this.controls.getObject().position.z - 1
+        )
             
         file.update({id: "player",
             position:{
-            x:this.controls.getObject().position.x,
-            y:this.controls.getObject().position.y,
-            z:this.controls.getObject().position.z},
+                x:this.controls.getObject().position.x,
+                y:this.controls.getObject().position.y,
+                z:this.controls.getObject().position.z
+            },
             rotation_euler:{
                 x:this.controls.getObject().rotation.x,
                 y:this.controls.getObject().rotation.y,
                 z:this.controls.getObject().rotation.z
-            }});
+            }})
     }
 
     set_transform(t){
-        this.transform = t;
+        this.transform = t
  
         this.parent.transform.position = 
         new Vector3(
@@ -101,7 +103,7 @@ export class controller extends component {
     }
     set_requirement(r){
         if(r.type === "transform"){
-            this.set_transform(r);
+            this.set_transform(r)
         }
     }
 }
