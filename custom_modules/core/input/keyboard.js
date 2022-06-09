a554d70e0c8aea37c55fe09db6dafa6b3fd4308d
Togene <jhonnywhiplash@hotmail.com.au>
2022-06-09 22:58:15 +1000

    Keyboard events started!

--- custom_modules/core/input/keyboard.js
+++ custom_modules/core/input/keyboard.js
@@ -18,29 +18,46 @@ export const input = {
     "Esc" : false,
 }
 
+let input_events = new Map()
+let hot_key_name = "hot-key"
+
 // init keyboard by creating event listeners
 export const init = () => {
     document.addEventListener( 'keydown', onKeyDown, false )
     document.addEventListener( 'keyup', onKeyUp, false )
+
+    for (let key in get_input_meta()) {
+        input_events.set(get_input_meta()[key][hot_key_name], new Event(key))
+        addEventListener(key, function (e) {
+            console.log("\t"+key + " event")
+        }, false)
+    }
+
+    console.log(input_events)
     console.log("%cKeyboard Initialized", "color:#61AFEF")
 }
 
 const onKeyDown = (e) => {
     let input_key= input[keyCodeToChar[e.keyCode]]
-    if (input_key== undefined) {
-        console.log(keyCodeToChar[e.keyCode] + " - not mapped!")
+    if (input_key == undefined) {
+        //console.log(keyCodeToChar[e.keyCode] + " - not mapped!")
     } else {
-        console.log(keyCodeToChar[e.keyCode] + " - Down")
+
+        if (input_events.get(keyCodeToChar[e.keyCode]) != undefined) {
+            dispatchEvent(input_events.get(keyCodeToChar[e.keyCode]))
+        }
+
+        //console.log(keyCodeToChar[e.keyCode] + " - Down")
         input[keyCodeToChar[e.keyCode]] = true
     }
 }
 
 const onKeyUp = (e) => {
     let input_key= input[keyCodeToChar[e.keyCode]]
-    if (input_key== undefined) {
-        console.log(keyCodeToChar[e.keyCode] + " - not mapped!")
+    if (input_key == undefined) {
+        //console.log(keyCodeToChar[e.keyCode] + " - not mapped!")
     } else {
-        console.log(keyCodeToChar[e.keyCode]  + " - Up")
+        //console.log(keyCodeToChar[e.keyCode]  + " - Up")
         input[keyCodeToChar[e.keyCode]] = false
     }
 }
